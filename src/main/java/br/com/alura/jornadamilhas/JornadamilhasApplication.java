d2fc7b9d2a18186df88065ed44fc2680c202f55d
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-06-16 12:06:40 -0300

    projeto inicial

---
+++ src/main/java/br/com/alura/jornadamilhas/JornadamilhasApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.jornadamilhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornadamilhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornadamilhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/jornadamilhas/JornadamilhasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.jornadamilhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadamilhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
